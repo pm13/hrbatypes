bc5ac3163b6b2b7d3be6de9f6a44ff6cc61268a7
Petr Marhoun <petr.marhoun@gmail.com>
2026-09-01 19:58:17 +0200

    remove unused jinja filters

--- pelicanconf.py
+++ pelicanconf.py
@@ -54,31 +54,11 @@ FEED_MAX_ITEMS = 10
 FEED_ALL_ATOM = CATEGORY_FEED_ATOM = AUTHOR_FEED_ATOM = AUTHOR_FEED_RSS = TRANSLATION_FEED_ATOM = None
 
 
-def get_category_key(category):
-    name = category[0].name
-    if name.startswith('Léto'):
-        return 0, name
-    elif name.startswith('Zima'):
-        return -1, name
-    else:
-        return -2, name
-
-
 def get_article_key(article):
     sticky = getattr(article, 'sticky', '0')
     return bool(int(sticky)), article.date
 
 
-def postprocess_content(content, smaller_headers=False):
-    content = content.replace('<h2>Přílohy:</h2>', '<h2 class="text-lg">Přílohy:</h2>')
-    if smaller_headers:
-        content = content.replace('<h2>', '<h2 class="h3">')
-        content = content.replace('<h3>', '<h3 class="h4">')
-    return content
-
-
 JINJA_FILTERS = {
-    'sort_categories': partial(sorted, key=get_category_key, reverse=True),
     'sort_articles': partial(sorted, key=get_article_key, reverse=True),
-    'postprocess_content': postprocess_content,
 }
